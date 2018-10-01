64a5cc9556eb3e55b21107d52e091e2b8868c930
Nani St <nani-soto@hotmail.com>
2018-10-01 01:34:14 +0000

    now works

--- http_example.js
+++ http_example.js
@@ -7,11 +7,15 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+// called by https when the request is made.
+var callback = function(response) {
   console.log('In response handler callback!');
-}
 
-console.log("I'm about to make the request!");
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+};
 
 https.request(options, callback).end();
 
